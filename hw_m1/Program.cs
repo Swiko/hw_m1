f684a74ad2876cafd10d684c20202bc40abf0cc8
respa <respa@DESKTOP-OOC8PJL>
2022-03-14 22:36:04 +0300

    Добавьте файлы проекта.

---
+++ hw_m1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw_m1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // first task
+
+            WriteLine("Hello World !!!\n");
+
+            // second task
+
+            Write("Hello ");
+            Write("World ");
+            Write("!!!");
+            
+            ReadKey();
+
+        }
+    }
+}
